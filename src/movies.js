d5d82eeccc6c703b4280dceb17df680cc5611036
IVANIVALDO <142198929+IVANIVALDO@users.noreply.github.com>
2025-06-22 09:51:29 -0300

    fix: disable max-len only on long URL string lines

--- src/movies.js
+++ src/movies.js
@@ -5,6 +5,7 @@ export const moviesFromServer = [
     title: 'The Shawshank Redemption',
     description: 'Two imprisoned men bond over a number of years...',
     posterUrl:
+      // eslint-disable-next-line max-len
       'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmRhMC00ZjQ5LWFmNTEtODM1ZmRlY2Zj' +
       'MjUxXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_.jpg',
   },
@@ -13,6 +14,8 @@ export const moviesFromServer = [
     title: 'The Godfather',
     description: 'The aging patriarch of an organized crime dynasty...',
     posterUrl:
-      'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmYtYTAwOC00ZjQ1LWFmNTEtODM1ZmRlY2ZjMjUxXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg',
+      // eslint-disable-next-line max-len
+      'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmYtYTAwOC00ZjQ1LWFmNTEtODM1ZmRlY2Zj' +
+      'MjUxXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg',
   },
 ];
